9c17306d1573c9eeca5c2343b08e219ebe8ba3b9
sxh <997854244@qq.com>
2017-12-20 12:03:14 +0800

    修改webpack的配置

--- upload/js/index.js
+++ upload/js/index.js
@@ -1,16 +1,16 @@
-let event = require("../../unit/event");
+let eventUnit = require("../../unit/eventUnit");
 let dom = require("../../unit/dom");
 
 let bindDrop = function() {
     let div = dom.getDom("#drop_area");
-    event.addEvent(div, "dragenter", function(e) {
+    eventUnit.addEvent(div, "dragenter", function(e) {
         this.style.border = "1px solid #333";
     });
-    event.addEvent(div, "dragleave", function(e) {
+    eventUnit.addEvent(div, "dragleave", function(e) {
         this.style.border = "1px dashed #dbdbdb";
     });
 
-    event.addEvent(div, "drop", function(e) {
+    eventUnit.addEvent(div, "drop", function(e) {
         e.preventDefault();
         e.stopPropagation();
         let dt = e.dataTransfer;
@@ -21,17 +21,17 @@ let bindDrop = function() {
     /**
      * 阻止浏览器默认打开文件
      */
-    event.addEvent(document, "dragenter", function(e) {
+    eventUnit.addEvent(document, "dragenter", function(e) {
         e.preventDefault();
     });
-    event.addEvent(document, "dragleave", function(e) {
+    eventUnit.addEvent(document, "dragleave", function(e) {
         e.preventDefault();
     });
 
-    event.addEvent(document, "drop", function(e) {
+    eventUnit.addEvent(document, "drop", function(e) {
         e.preventDefault();
     });
-    event.addEvent(document, "dragover", function(e) {
+    eventUnit.addEvent(document, "dragover", function(e) {
         e.preventDefault();
     });
     /**
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './upload/js/index.js',
+    entry: {
+        app: './src/index.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/,
